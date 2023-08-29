8c03ead67e7d18f210a7926460e00fa0040dde55
CamissCa <camila.cardososenai@gmail.com>
2023-08-28 21:55:50 -0300

    8.0.2

--- ProjetoPM/Carrinho.Designer.cs
+++ ProjetoPM/Carrinho.Designer.cs
@@ -28,24 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.FinCompra = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
             this.SuspendLayout();
             // 
-            // button1
+            // FinCompra
             // 
-            this.button1.Location = new System.Drawing.Point(798, 415);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(164, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.FinCompra.Location = new System.Drawing.Point(798, 415);
+            this.FinCompra.Name = "FinCompra";
+            this.FinCompra.Size = new System.Drawing.Size(164, 23);
+            this.FinCompra.TabIndex = 0;
+            this.FinCompra.Text = "Finalizar Compra";
+            this.FinCompra.UseVisualStyleBackColor = true;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(30, 30);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(932, 379);
+            this.checkedListBox1.TabIndex = 0;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
             // 
             // Carrinho
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(974, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.FinCompra);
             this.Name = "Carrinho";
             this.Text = "Carrinho";
             this.ResumeLayout(false);
@@ -54,6 +65,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button FinCompra;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
     }
 }
